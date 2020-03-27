25bcb7ce99ebe97a275ea48c235942c2742e0ac5
nitunwa <nitun.2@gmail.com>
2020-03-27 13:53:55 -0700

    add business logic to js

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,28 @@
 
 
-
+var answerList = []
 $(document).ready(function(){
 
+  $("#sec-1").hide();
   $("#sec-2").hide();
   $("#sec-3").hide();
 });
 
-function showNextQuiz(id){
+function showNextQuiz(id, answer){
+
+  var currentSection = id-1
+  var nextSection = id
+  answerList[currentSection]=answer;
+  console.log("currentSection:" +currentSection)
+  $("#sec-"+currentSection).hide();
+  $("#sec-"+nextSection).show();
+
+  if(nextSection==3){
+       
+      var result = "C#"
+      //if(answer[0]==='dev'an)
+      $("#result").text(result)
+  }
 
 
-  console.log("id" +id)
-  $("#sec-"+(id-1)).hide();
-  
-  $("#sec-"+id).show();
 }
