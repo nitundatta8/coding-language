72df1464ef59139dfb212f738a1fcfb81517c855
nitunwa <nitun.2@gmail.com>
2020-03-27 16:47:29 -0700

    add servey pages

---
+++ README.md
@@ -0,0 +1,22 @@
+# Programming Language Suggester
+### Made by Nitun Datta
+**Html,Css,Bootstrap,Jquery project for Epicodus, 03.27.2020**
+## Description
+#### Create an application for coding to decide which language people should learn first.
+
+## Setup
+  - Clone this repository.
+  - View in browser
+  - [Link](https://github.com/nitundatta8/coding-language/tree/gh-pages)
+
+### Technology Used
+  - Html
+  - Css
+  - Bootstrap
+  - Markdown
+  - Jquery
+
+### Legal 
+**Copyright (c) 2020**
+
+**This software is licensed under the MIT license**. 
---
+++ js/scripts.js
@@ -0,0 +1,48 @@
+
+var answerList = []
+$(document).ready(function(){
+
+  $("#sec-1").hide();
+  $("#sec-2").hide();
+  $("#sec-3").hide();
+  $("#sec-4").hide();
+  $("#sec-5").hide();
+  
+});
+
+function showNextQuiz(id, answer){
+
+  var currentSection = id-1
+  var nextSection = id
+  answerList[currentSection]=answer;
+  console.log("currentSection:" +currentSection)
+  $("#sec-"+currentSection).hide();
+  $("#sec-"+nextSection).show();
+
+  if(nextSection==5){
+
+    //1 --> developer  startup fun
+//2 Web Mobile Desktop
+// 3 UX Logic
+//4 Windows Mac CrosPlatForm
+//5 Yes No
+       //"developer", "Web", "Logic", "Mac", "Yes"]
+console.log(answerList)
+      var result = "C#"
+      if(answerList[0]==='developer' && answerList[1]==='Web' && answerList[2]==='UX' ){
+        result="Since You like Web Development and UI, Javascript is good for you"
+      }else if(answerList[0]==='fun' && answerList[1]==='Web' &&  answerList[2]==='UX' ){
+        result="For fun try HTML5, BootStrap and CSS"
+      }else if(answerList[0]==='developer' &&  answerList[2]==='Logic'  && answerList[4]==='No'){
+        result="Since you want to backend developer, C# and SQL is good for you"
+      }else if(answerList[0]==='developer' &&  answerList[2]==='Logic' && answerList[4]==='Yes' ){
+        result="Since you have experience and want to be  backend developer,"+
+        " Java, Javascript, BootStrap and SQL is good for you"
+      }else{
+        result="You can start with HTML5, CSS, Bootstrap and Javascript";
+      }
+      $("#result").text(result)
+  }
+
+
+}
